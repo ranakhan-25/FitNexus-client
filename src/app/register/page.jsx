12cc1c409117d940ff6b0c60b96c2bd6108709cf
Ranadev25 <ranakhandev2025@gmail.com>
2026-06-23 04:56:36 +0600

    modify signin with google btn

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -59,7 +59,6 @@ export default function RegisterPage() {
 
     const finalData = { ...userData, role, image: imageUrl };
 
-
     const { data, error } = await authClient.signUp.email({
       email: finalData.email,
       password: finalData.password,
@@ -76,6 +75,12 @@ export default function RegisterPage() {
       toast.success("Register successfully");
       router.push(`/`);
     }
+  };
+
+  const handleGoogleLogin = async () => {
+    const data = await authClient.signIn.social({
+      provider: "google",
+    });
   };
 
   return (
@@ -203,6 +208,34 @@ export default function RegisterPage() {
             <Check />
             Create Account
           </Button>
+
+          {/* GOOGLE LOGIN */}
+          <button
+            type="button"
+            onClick={handleGoogleLogin}
+            className="w-full flex items-center justify-center gap-2 border border-gray-300 dark:border-gray-700 rounded-lg py-2 my-2 hover:bg-gray-100 dark:hover:bg-gray-800 transition"
+          >
+            {/* Google Icon */}
+            <svg width="20" height="20" viewBox="0 0 48 48">
+              <path
+                fill="#EA4335"
+                d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.69 30.4 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.14 17.73 9.5 24 9.5z"
+              />
+              <path
+                fill="#4285F4"
+                d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"
+              />
+              <path
+                fill="#FBBC05"
+                d="M10.54 28.41a14.5 14.5 0 0 1 0-9.82l-7.98-6.19A23.9 23.9 0 0 0 0 24c0 3.84.92 7.47 2.56 10.6l7.98-6.19z"
+              />
+              <path
+                fill="#34A853"
+                d="M24 48c6.4 0 11.8-2.1 15.73-5.73l-7.73-6c-2.15 1.45-4.92 2.3-8 2.3-6.27 0-11.57-3.64-13.46-8.89l-7.98 6.19C6.51 42.62 14.62 48 24 48z"
+              />
+            </svg>
+            Continue with Google
+          </button>
         </Form>
         <Link href="/login" className="text-sm text-center block mt-3">
           Already have account? <span className="text-green-500">Login</span>
